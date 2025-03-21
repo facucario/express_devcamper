a953f0c0468db3d7a8c1e79791fde3f017063b26
Facundo Cario <facundocario@gmail.com>
2025-03-21 20:48:23 -0300

    Add review model

--- seeder.js
+++ seeder.js
@@ -32,6 +32,7 @@ const importData = async () => {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
+    await Review.create(users);
     console.log("Data imported...");
     process.exit();
   } catch (err) {
